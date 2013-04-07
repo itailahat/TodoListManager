3276f436ec3c65af3f9b80401fa108c452569206
Itai Lahat <itai.lahat.il@gmail.com>
2013-04-07 21:44:36 +0300

    Ex4 start

--- TodoListManager/src/il/ac/huji/todolist/TodoItem.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoItem.java
@@ -2,7 +2,7 @@ package il.ac.huji.todolist;
 
 import java.util.Date;
 
-public class TodoItem {
+public class TodoItem implements ITodoItem{
 	public String title;
 	public Date dueDate;
 	
@@ -10,6 +10,16 @@ public class TodoItem {
 		super();
 		this.title = title;
 		this.dueDate = dueDate;
+	}
+
+	@Override
+	public String getTitle() {
+		return title;
+	}
+
+	@Override
+	public Date getDueDate() {
+		return dueDate;
 	}
 	
 	
